b5c5eae31476342362431cd6715d754384ac6934
amaltsanthosh01 <amal.ts@gemsessence.com>
2024-08-13 19:47:46 +0530

    Created a new MyContract.

---
+++ contracts/MyContract.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0; // or whichever version of Solidity you're using
+contract MyContract {
+    string public ourString = "Hello World on Earth";
+}
