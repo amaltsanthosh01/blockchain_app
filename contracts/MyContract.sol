00eb94f880fa6bcafa4c0d7318eb26f2acf61d1f
amaltsanthosh01 <amal.ts@gemsessence.com>
2024-08-13 19:57:00 +0530

    Added a function to update the string.

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -2,4 +2,8 @@
 pragma solidity >=0.7.0 <0.9.0; // or whichever version of Solidity you're using
 contract MyContract {
     string public ourString = "Hello World on Earth";
+
+    function updateOurString(string memory _updatedString) public {
+        ourString = _updatedString;
+    }
 }
