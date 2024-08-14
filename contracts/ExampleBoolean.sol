a70a724c3d996cd27ca6f13519e15c88eb48cab8
amaltsanthosh01 <amal.ts@gemsessence.com>
2024-08-14 12:08:54 +0530

    Update class name of example boolean

--- contracts/ExampleBoolean.sol
+++ contracts/ExampleBoolean.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
 
-contract MyBoolean {
+contract ExampleBoolean {
     bool public myBool;
 
     function updateMyBool(bool _myBool) public {
